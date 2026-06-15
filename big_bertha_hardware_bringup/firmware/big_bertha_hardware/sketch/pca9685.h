1e76eb477ccd7ee847b951915addd1a0a28cc683
Yorha-7 <scientistn1420@gmail.com>
2026-06-15 10:10:57 +0530

    fix: conflict

--- big_bertha_hardware_bringup/firmware/big_bertha_hardware/sketch/pca9685.h
+++ big_bertha_hardware_bringup/firmware/big_bertha_hardware/sketch/pca9685.h
@@ -4,6 +4,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+<<<<<<< HEAD
 #define PCA9685_ADDR 0x40
 #define PCA9685_MODE1 0x00
 #define PCA9685_PRESCALE 0xFE
@@ -23,6 +24,27 @@ private:
   uint16_t _periodUs = 20000;
 
   void writeReg(uint8_t reg, uint8_t val);
+=======
+#define PCA9685_ADDR        0x40
+#define PCA9685_MODE1       0x00
+#define PCA9685_PRESCALE    0xFE
+#define PCA9685_LED0_ON_L   0x06
+#define PCA9685_CHANNELS    16
+
+class PCA9685 {
+public:
+    bool begin(TwoWire &wire = Wire);
+    void setPulse(uint8_t channel, uint16_t pulseUs);
+    void setAllPulses(const uint16_t *pulseUs, uint8_t count);
+    void setPWMFreq(uint16_t freq);
+
+private:
+    TwoWire *_wire;
+    uint8_t  _addr = PCA9685_ADDR;
+    uint16_t _periodUs = 20000;
+
+    void writeReg(uint8_t reg, uint8_t val);
+>>>>>>> fd42369 (feat: MPU6050 and PAC9685 hardware interface for big_bertha)
 };
 
 #endif
